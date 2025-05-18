d20bc4c749a0f711b3f7b5826cc571cfdd92c480
Nushbhatt <anushkabhattacherjee@gmail.com>
2025-05-18 15:14:10 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,24 @@
+a=input('Enter the first number: ')
+b=input('Enter the second number: ')
+sum=int(a)+int(b)
+print('Addition =',sum)
+diff=int(a)-int(b)
+print('Subtraction =',diff)
+multiplication=int(a)*int(b)
+print('Multiplication =',multiplication)
+division=int(a)/int(b)
+print('Division =',division)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task 2.py
@@ -0,0 +1,3 @@
+x=input('Enter the first name: ')
+y=input('Enter the second name: ')
+print('Hello, John Doe ! Welcome to the Python Program')
